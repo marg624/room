7da141b845237d75fc944137df6fa78b44d72d01
Margarita Miranda <marg624@gmail.com>
2012-10-09 00:17:24 -0400

    myql update php file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php
 $conn =  new mysqli('localhost', 'room_user', 'password', 'room', '');
-$result = $conn->query("SELECT name FROM users;");
+$result = $conn->query("SELECT * FROM users;");
 $row = $result->fetch_assoc();
 echo $row['name'];
 ?>

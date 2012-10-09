a3644e307e124c8acdff648aa0be01e30aaf0e6a
Margarita Miranda <marg624@gmail.com>
2012-10-09 00:25:57 -0400

    myql update php file

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-$conn =  new mysqli('localhost', 'room_user', 'password', 'room', '');
+$conn =  new mysqli('54.243.183.105', 'room_user', 'password', 'room', '');
+if (!$conn) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'connection successful';
 $result = $conn->query("SELECT * FROM users;");
 $row = $result->fetch_assoc();
 echo $row['name'];

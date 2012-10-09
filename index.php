5430452fe5ebe73579bd6ff481edd7525b37a825
Margarita Miranda <marg624@gmail.com>
2012-10-09 00:32:07 -0400

    myql update php file

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ if (!$conn) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'connection successful';
-$result = $conn->query("SELECT * FROM users;");
+$result = $conn->mysql_query("SELECT * FROM users")
+	or die ("Unable to run query");
+$nresult = mysql_num_rows($result);
 $row = $result->fetch_assoc();
+echo '$nresult returned:';
 echo $row['name'];
 ?>
 
